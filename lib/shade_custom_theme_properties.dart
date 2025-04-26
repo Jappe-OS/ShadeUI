7e008c1c41a5fd83bcd4d9b43f1c2fc6a8ff7843
Jappe02 <jasper.mannonen@gmail.com>
2025-04-26 23:21:11 +0300

    Fix provider

--- lib/shade_custom_theme_properties.dart
+++ lib/shade_custom_theme_properties.dart
@@ -15,6 +15,7 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'shade_theme_primary.dart';
 
